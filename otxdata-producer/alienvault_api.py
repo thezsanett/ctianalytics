c4a07478fa67218290d12d7fdded39c0a339e95e
Armbruszt Zsanett (U753540) <zsanett.armbruszt@lhsystems.com>
2022-11-15 17:31:36 +0100

    Getting only active indicators

--- otxdata-producer/alienvault_api.py
+++ otxdata-producer/alienvault_api.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 
 otx = OTXv2("a1c6e949d849b28592e0f25ebbd6d05c4cb49d28f442c96f45d5342874e4c286")
 
-indicators = otx.get_pulse_indicators("602bc528f447d628d41494f2", include_inactive=True)
+indicators = otx.get_pulse_indicators("602bc528f447d628d41494f2", include_inactive=False)
 
 #TODO: get more data like this?
 #otx.get_indicator_details_full(IndicatorTypes.DOMAIN, "knucker.io")
