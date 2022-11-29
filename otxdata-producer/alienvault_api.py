ea4d0c421b625a363a2e0991070c9ac2bb40330a
Zsanett Armbruszt <sz2jjs@inf.elte.hu>
2022-11-29 22:02:54 +0100

    Alienvault empty default

--- otxdata-producer/alienvault_api.py
+++ otxdata-producer/alienvault_api.py
@@ -3,30 +3,51 @@ from kafka import KafkaProducer
 from time import sleep
 from datetime import datetime
 import os, uuid, json
+
 SLEEP_TIME = int(os.environ.get("SLEEP_TIME", 5))
 producer = KafkaProducer(bootstrap_servers=["broker:9092"], api_version=(0, 10, 1))
+empty_detail = {
+  'geo': {
+    'latitude': -1,
+    'longitude': -1,
+    'accuracy_radius': -1,
+  },
+  'url_list': {
+    'url_list': []
+  }
+}
+
 pulses = [
   "602bc528f447d628d41494f2",
   "617b089bbbab1cd42903ced7"
 ]
+
 otx = OTXv2("a1c6e949d849b28592e0f25ebbd6d05c4cb49d28f442c96f45d5342874e4c286")
 # otx = None
+
 def get_details(indicator):
-  if indicator['type'] == 'IPv4':
-    otx.get_indicator_details_full(IndicatorTypes.IPv4, indicator["indicator"])
-  elif indicator['type'] == 'Domain':
-    otx.get_indicator_details_full(IndicatorTypes.DOMAIN, indicator["indicator"])
-  else:
-    print(type)
+  try:
+    if indicator['type'] == 'IPv4':
+      return otx.get_indicator_details_full(IndicatorTypes.IPv4, indicator["indicator"])
+    if indicator['type'] == 'Domain':
+      return otx.get_indicator_details_full(IndicatorTypes.DOMAIN, indicator["indicator"])
+    return empty_detail
+  except:
+    print('Detail for pulse ' + indicator['indicator'] + ' can not be found.')
+    return empty_detail
+
 def json_serializer(data):
   return json.dumps(data).encode("utf-8")
+
 def get_and_send_pulse(otx, pulse_id, generated_id):
-  indicators = otx.get_pulse_indicators(pulse_id, include_inactive=False)
-  # indicators = [
-  #   {'indicator': '69.73.130.198', 'type': 'IPv4'},
-  #   {'indicator': 'aoldaily.com', 'type': 'Domain'}
-  # ]
+  # indicators = otx.get_pulse_indicators(pulse_id, include_inactive=False)
+  indicators = [
+    {'indicator': '69.73.130.198', 'type': 'IPv4'},
+    {'indicator': 'aoldaily.com', 'type': 'Domain'}
+  ]
+
   print("Pulse acquired.")
+
   for data in indicators:
     details = get_details(data)
     data['latitude'] = details['geo']['latitude']
@@ -35,9 +56,12 @@ def get_and_send_pulse(otx, pulse_id, generated_id):
     data['url_list_length'] = len(details['url_list']['url_list'])
     data['pulse_id'] = generated_id
     data['created'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
     producer.send('alienvaultdata', json_serializer(data))
+
     print("Sent: ", data)
     sleep(5)
+
 for pulse in pulses:
   generated_id = str(uuid.uuid1())
   get_and_send_pulse(otx, pulse, generated_id)
