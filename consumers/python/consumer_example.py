b318989b133335e26f8b2c3ef0f61de771a4e6b0
Zsanett Armbruszt <sz2jjs@inf.elte.hu>
2022-12-02 08:28:10 +0100

    First real sketches

--- consumers/python/consumer_example.py
+++ consumers/python/consumer_example.py
@@ -1,51 +1,128 @@
 from kafka import KafkaConsumer
 from time import sleep
+from random import randrange
 import json
 
-# Set up variables
+####################
+# Set up variables #
+####################
 consumer = None
 counter = 0
 
-# Set up methods
+##################
+# Set up methods #
+##################
 def create_consumer():
     consumer = KafkaConsumer('alienvaultdata', bootstrap_servers='broker:9092')
     return consumer
 
-# Sketch variables
-accuracy_radius = 0
+####################
+# Sketch variables #
+####################
+### Common
+num_data = 0            # number of data points seen so far
 
-# TODO: add more variables
+### Moving average of the accuracy radius
+MA_acc_rad = 0          # moving average 
+MA_n = 5                # n used in MA 
+MA_x_array = [0] * MA_n # n long array
 
-# Consumer sketches
+### Exponenential average of the accuracy radius
+EMA_alpha = 0.6          # weight of the last point
+EMA_last_acc_rad = 0     # moving average of the previous timestamp
+EMA_acc_rad = 0          # current exponential moving average
+
+### Reservoir sampling
+RS_k = 5                 # array length
+RS_array = []            # reservoir array
+
+#####################
+# Consumer sketches #
+#####################
+### Common
 def consume_write(message_value):
-    print(message_value)
+    global num_data
+    num_data += 1
+    print(num_data, message_value)
 
-def consume_add_accuracy_radius(message_value):
-    global accuracy_radius
-    accuracy_radius += int(message_value['accuracy_radius'])
-    print("\nAccuracy radius:", accuracy_radius, '\n')
+### Moving average of the accuracy radius
+def consume_MA_accuracy_radius(message_value):
+    global MA_acc_rad, MA_n, MA_x_array, num_data, EMA_last_acc_rad
+
+    EMA_last_acc_rad = MA_acc_rad
+    xt = message_value['accuracy_radius']
+    oldest_ind = (num_data - MA_n + 1) % MA_n
+
+    MA_acc_rad = MA_acc_rad - (MA_x_array[oldest_ind] / num_data) + (xt / num_data)
+    MA_acc_rad = round(MA_acc_rad, 2)
+
+    MA_x_array[oldest_ind] = xt
+    print('Moving average of 5 of the accuracy radius:', MA_acc_rad)
+
+### Exponential moving average of the accuracy radius
+def consume_EMA_accuracy_radius(message_value):
+    global EMA_alpha, EMA_acc_rad, EMA_last_acc_rad
+
+    xt = message_value['accuracy_radius']
+    EMA_acc_rad = EMA_alpha * xt + (1-EMA_alpha) * EMA_last_acc_rad
+    EMA_acc_rad = round(EMA_acc_rad, 2)
+
+    print('Exponential moving average of 5 of the accuracy radius:', EMA_acc_rad)
+
+### Reservoir sampling
+def create_reservoir_object(message_value):
+    return {
+        'indicator': message_value['indicator'],
+        'latitude': message_value['latitude'],
+        'longitude': message_value['longitude']
+    }
+
+def consume_reservoir_sampling(message_value):
+    global RS_k, RS_array
+
+    reservoir_object = create_reservoir_object(message_value)
+    t = num_data-1
+
+    if  t < RS_k:
+        print('Reservoir sample in initalization phase')
+        RS_array.append(reservoir_object)
+    
+    else:
+        r = randrange(RS_k)
+        if r < RS_k:
+            print('Reservoir sample in update phase replacing index', r)
+            RS_array[r] = reservoir_object
+        
+    print('Reservoir sample', RS_array)
 
 # TODO: add more sketches
 
-# Main app
+############
+# Main app #
+############
 while counter < 10 and consumer is None:
     try:
         print('Trying to connect to Kafka...')
         consumer = create_consumer()
     except:
-        print("Kafka is not reachable yet.")
+        print('Kafka is not reachable yet.')
         counter += 1
-        sleep(15)
+        sleep(10)
 
 if consumer is not None:
-    print("Kafka is up. Printing messages on topic alienvaultdata...")
+    print('Kafka is up. Printing messages on topic alienvaultdata...')
 
     for msg in consumer:
         message_value = json.loads(msg.value)
-        consume_write(message_value)
-        consume_add_accuracy_radius(message_value)
 
-    print("Done.")
+        consume_write(message_value)
+        consume_MA_accuracy_radius(message_value)
+        consume_EMA_accuracy_radius(message_value)
+        consume_reservoir_sampling(message_value)
+
+        print(' ')
+
+    print('Consumer process successfully ended.')
 
 else:
-    print("Consumer can not be initialized. Exiting.")
+    print('Consumer can not be initialized. Exiting.')
