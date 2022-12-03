39200dda5acea1aa9d9b0dbcbd6ddd8d7434b112
Szolár Balázs <leftazshun@gmail.com>
2022-12-03 12:41:01 +0100

    Fix pom.xml dependency issues of 9b488f7

--- stop.bat
+++ stop.bat
@@ -2,6 +2,6 @@
  docker-compose -f kafka/docker-compose.yml down
  docker-compose -f cassandra/docker-compose.yml down
  docker-compose -f consumers/docker-compose.yml down
- docker-compose -f flink/flink-compose.yml down
+ docker-compose -f flink/docker-compose.yml down
  docker network rm kafka-network
  docker network rm cassandra-network
